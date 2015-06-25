7523af9e8a6061896d5b060193773c6382bfd935
sarahbes <sarah.besnainou@gmail.com>
2015-06-25 18:32:51 +0200

    Lekker Plekke day 9

---
+++ SarahBesnainou-sarahbes/d8/lekker_plekke/app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+	  def create
+    @place = Place.find(params[:place_id])
+    @comment = @place.comments.create(comment_params)
+    redirect_to place_path(@place)
+  end
+ 
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+
+end
